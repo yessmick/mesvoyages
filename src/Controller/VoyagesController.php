2339b4f8aa14db0ace8636bce523ec4248095bfc
Nour <nour.adlaoui15@gmail.com>
2026-01-22 01:50:49 +0100

    Phase 9 : ORM (filtrage des enregistrements).

--- src/Controller/VoyagesController.php
+++ src/Controller/VoyagesController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\VisiteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -32,6 +33,15 @@ class VoyagesController extends AbstractController{
     #[Route('/voyages/tri/{champ}/{ordre}', name: 'voyages.sort')]
     public function sort($champ, $ordre): Response{
         $visites = $this->repository->findAllOrderBy($champ, $ordre);
+        return $this->render("pages/voyages.html.twig", [
+            'visites' => $visites
+        ]);
+    }
+    
+    #[Route('/voyages/recherche/{champ}', name: 'voyages.findallequal')]
+    public function findAllEqual($champ, Request $request): Response{
+        $valeur = $request->get("recherche");
+        $visites = $this->repository->findByEqualValue($champ, $valeur);
         return $this->render("pages/voyages.html.twig", [
             'visites' => $visites
         ]);
--- src/Repository/VisiteRepository.php
+++ src/Repository/VisiteRepository.php
@@ -24,6 +24,29 @@ class VisiteRepository extends ServiceEntityRepository
                 ->getQuery()
                 ->getResult();
     }
+    
+    /**
+     * Enregistrements dont un champ est égal à une valeur
+     * ou tous les enregistrements si la valeur est vide
+     * @param type $champ
+     * @param type $valeur
+     * @return Visite[]
+     */    
+    public function findByEqualValue($champ, $valeur) : array {
+        if($valeur==""){
+            return $this->createQueryBuilder('v') // alias de la table
+                ->orderBy('v.'.$champ, 'ASC')
+                ->getQuery()
+                ->getResult();
+        } else{
+            return $this->createQueryBuilder('v') // alias de la table
+                ->where('v.'.$champ. '=:valeur')
+                ->setParameter('valeur', $valeur)
+                ->orderBy('v.datecreation', 'DESC')
+                ->getQuery()
+                ->getResult();
+        }
+    }
  
     public function __construct(ManagerRegistry $registry)
     {
